a5bdea27d4b5add19b2c9df052a47ed00f8a257f
Byealha <junpll0527@naver.com>
2026-04-02 21:13:43 +0900

    test2

--- Assets/Scripts/test/Test.cs
+++ Assets/Scripts/test/Test.cs
@@ -5,7 +5,7 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("ÀÌÁØÈ£ ¹Ùº¸");
+        Debug.Log("ê¹€ì˜í›ˆ ì¼€ì¼€");
     }
 
     
