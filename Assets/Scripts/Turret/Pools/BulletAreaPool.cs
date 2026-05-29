47c94d85b3d0641840750948b19c56e61a7b18b6
Byealha <junpll0527@naver.com>
2026-05-29 21:10:38 +0900

    Add area turret / Fix turret when install gatling and sniper bug

---
+++ Assets/Scripts/Turret/Pools/BulletAreaPool.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAreaPool : MonoBehaviour
+{
+    [Header("총알")]
+    [SerializeField] private GameObject bulletAreaPrefab;
+    [SerializeField] private int maxBullets = 10;
+    
+    private readonly List<GameObject> pool =  new List<GameObject>();
+
+    private void Awake()
+    {
+        for (int i = 0; i < maxBullets; i++)
+        {
+            GameObject obj = Instantiate(bulletAreaPrefab, transform);
+            obj.SetActive(false);
+            pool.Add(obj);
+        }
+    }
+
+    public GameObject GetBullet()
+    {
+        for (int i = 0; i < pool.Count; i++)
+        {
+            if (!pool[i].activeSelf)
+            {
+                return pool[i];
+            }
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Scripts/Turret/TurretArea.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class TurretArea : TurretBase
+{
+    [SerializeField] private TurretAreaData turretAreaData;
+    [SerializeField] private BulletAreaPool bulletAreaPool;
+    [SerializeField] private Transform shootPoint;
+
+    [SerializeField] private bool isFind = false;
+
+    private TurretStat turretStat;
+    private Transform _currentTarget;
+    private float _nextFireTime = 0f;
+
+    private void Awake()
+    {
+        turretStat = turretAreaData.RuntimeStat;
+    }
+
+    private void Update()
+    {
+        if (isFind) TryAttack();
+    }
+
+    private void TryAttack()
+    {
+        _currentTarget = FindFirstTarget(transform.position, turretStat.attackRange);
+
+        if (_currentTarget == null) return;
+
+        Attack(_currentTarget);
+    }
+
+    private void Attack(Transform target)
+    {
+        if(Time.time < _nextFireTime) return;
+
+        _nextFireTime = Time.time + turretStat.cooldown;
+        
+        GameObject bullet = bulletAreaPool.GetBullet();
+
+        if(bullet == null) return;
+
+        bullet.transform.position = shootPoint.position;
+        
+        Bullet bulletComponent = bullet.GetComponent<Bullet>();
+        bulletComponent.SetTarget(target, turretStat.shootingSpeed);
+
+        bullet.SetActive(true);
+    }
+}
