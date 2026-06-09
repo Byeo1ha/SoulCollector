d9a895f95438caaa5f52cd5031944475aaed7d9a
Byealha <junpll0527@naver.com>
2026-06-09 12:11:43 +0900

    Add map Image

--- Assets/Scripts/Build/TurretValidator.cs
+++ Assets/Scripts/Build/TurretValidator.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class TurretValidator : MonoBehaviour
 {
     [SerializeField] private LayerMask buildZoneLayer;
+    [SerializeField] private LayerMask roadZoneLayer;
     [SerializeField] private LayerMask turretLayer;
 
 #if UNITY_EDITOR
@@ -10,14 +11,19 @@ public class TurretValidator : MonoBehaviour
     private void AutoAssign()
     {
         buildZoneLayer = LayerMask.GetMask("BuildZone");
+        roadZoneLayer = LayerMask.GetMask("RoadZone");
         turretLayer = LayerMask.GetMask("Turret");
     }
 #endif
 
     public bool CanBuildZone(Vector2 vec)
     {
-        Collider2D hit = Physics2D.OverlapPoint(vec, buildZoneLayer);
-        return hit != null;
+        Collider2D buildHit = Physics2D.OverlapPoint(vec, buildZoneLayer);
+        if (buildHit == null)
+            return false;
+
+        Collider2D roadHit = Physics2D.OverlapPoint(vec, roadZoneLayer);
+        return roadHit == null;
     }
 
     public bool HasTurret(Vector2 vec)
