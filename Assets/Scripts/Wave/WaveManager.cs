1140517d14487b76925b0db8fa753c8c8c656e7d
sumer0608-53 <sumer0608@gmail.com>
2026-06-11 11:28:57 +0900

    10wave Error clear

--- Assets/Scripts/Wave/WaveManager.cs
+++ Assets/Scripts/Wave/WaveManager.cs
@@ -15,7 +15,7 @@ public class WaveManager : MonoBehaviour
     [SerializeField] private TurretBuildController turretBuildController;
     [SerializeField] private SFXClick sfxClick;
 
-    public int CurrentStage { get; private set; } = 1;
+    public int CurrentStage { get; private set; } = 10;
     public bool IsGameEnded => isGameEnded;
 
     private AudioSource audioSource;
@@ -56,7 +56,11 @@ public class WaveManager : MonoBehaviour
 
     public void UnregisterEnemy()
 {
+    Debug.Log(
+    $"CurrentStage={CurrentStage}, Alive={aliveEnemyCount}, IsSpawning={isSpawning}"
+    );
     aliveEnemyCount--;
+
 
     if (aliveEnemyCount > 0)
         return;
@@ -66,6 +70,7 @@ public class WaveManager : MonoBehaviour
 
     if (CurrentStage >= maxStage)
     {
+        Debug.Log("GameClear 진입");
         GameClear();
         return;
     }
